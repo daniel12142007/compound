f06c559b6b1bc28394b06171b1ee6a67dd243e69
murzakulova <aikenm310@gmail.com>
2023-02-14 17:23:00 +0600

    I add

--- src/main/java/org/example/model/Student.java
+++ src/main/java/org/example/model/Student.java
@@ -1,6 +1,47 @@
 package org.example.model;
 
-public class Student {
+import javax.persistence.*;
+import java.util.List;
 
+@Entity
+
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne( cascade = CascadeType.ALL)
+    @JoinColumn(name="course id")
+    private Student student;
+    public Student() {
+
+    }
+
+    public Student(Student student) {
+        this.student = student;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", student=" + student +
+                '}';
+    }
 }
 
